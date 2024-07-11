c88bed916076553b64130fdb8c86c5dece2be2ee
Shankarproject <gaddamshankar2002@gmail.com>
2024-07-11 16:54:11 +0530

    success

---
+++ ovel.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class ovel {
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		char ch;
+		System.out.println("Enter the character");
+		ch=sc.next().charAt(0);
+		switch(ch)
+		{
+		case  'A':
+		case  'a':
+		case  'E':
+		case  'e':
+		case  'I':
+		case  'i':
+		case  'O':
+		case  'o':
+		case  'U':
+		case  'u': 
+   
+                System.out.println("ovel");
+		
+break;                         
+		default: System.out.println("Consonent");
+		
+		
+		}
+		
+	}
+	
+
+}
+
